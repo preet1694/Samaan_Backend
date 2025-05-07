cc4d457f3beb937e14f821a2081d552f3605156a
1204Vraj <vrajranipa7@gmail.com>
2025-05-08 00:26:06 +0530

    updated backend for wallet

--- src/main/java/org/samaan/model/PaymentDetails.java
+++ src/main/java/org/samaan/model/PaymentDetails.java
@@ -14,7 +14,7 @@ public class PaymentDetails {
     private String status;
     private double paidAmount;
     private String paymentDate;
-
+    private String senderEmail;
     // Getters and Setters
     // ...
 }
